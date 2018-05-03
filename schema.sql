9ef6848e6942b10390f29811c70129727174dd48
Randolph <ampsternetwork@gmail.com>
2018-05-02 20:08:20 -0400

    trgefw

--- schema.sql
+++ schema.sql
@@ -13,4 +13,7 @@ CREATE TABLE products (
 
 
 INSERT INTO products (ID, Item, Price, Stock) VALUES (1,"Pickles",1.99,25);
+INSERT INTO products (Item, Department, Price, Stock)VALUES ("Mongo DB", "Electronics", 399, 1);
+INSERT INTO products (Item, Department, Price, Stock)VALUES ("React-JS", "Electronics", 299, 3);
+INSERT INTO products (Item, Department, Price, Stock)VALUES ("a real programming language", "Electronics", 60, 14);
 
